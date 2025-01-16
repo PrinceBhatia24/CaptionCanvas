ed748aea20cf57fa968daf6fb142f8bc067af435
Prince Bhatia <prinebhatia10021@gmail.com>
2025-01-17 00:12:45 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
 import './App.css'
-// import Homepage from './Pages/Homepage'
+import Homepage from './Pages/Homepage'
 
 function App() {
 
   return (
     <>
       <p className='text-white position-relative'>hhg</p>
-      {/* <Homepage /> */}
+      <Homepage />
     </>
   )
 }
