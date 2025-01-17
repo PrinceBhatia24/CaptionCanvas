d70c8d98536c306340ed713d530e589843db853f
Prince Bhatia <prinebhatia10021@gmail.com>
2025-01-17 09:54:18 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['pexels'], // Pre-bundle the pexels package
+    include: ['isomorphic-fetch'], // Pre-bundle the pexels package
   },
 })
